a5525c5f33bd76f959a6bc3cb1749372e01311d0
Jungho Yeom <junghoy@andrew.cmu.edu>
2016-11-07 23:12:11 -0500

    revised

--- edit-distance-tests.py
+++ edit-distance-tests.py
@@ -2,41 +2,38 @@
 
 import unittest
 from edit_distance import *
-import sys
 
-
-#Tests for hanoi.py
+#Tests for edit_distance.py
 
 class TestEditDistance(unittest.TestCase):	
-		#Tests whether same word edit returns 0
-		def test_same_word(self):
-			self.assertEqual(edit_distance('kevin', 'kevin'), 0)
-			self.assertEqual(edit_distance('john', 'john'), 0)
+	#Tests whether same word edit returns 0
+	def test_same_word(self):
+		self.assertEqual(edit_distance('kevin', 'kevin'), 0)
+		self.assertEqual(edit_distance('john', 'john'), 0)
 
-		#Tests substitution
-		def test_substitution(self):
-			self.assertEqual(edit_distance('back', 'jack'), 1)
-			self.assertEqual(edit_distance('kite', 'kate'), 1)
+	#Tests substitution
+	def test_substitution(self):
+		self.assertEqual(edit_distance('back', 'jack'), 1)
+		self.assertEqual(edit_distance('kite', 'kate'), 1)
 
-		#Tests insertion/deletion
-		def test_edit_insertion(self):
-			self.assertEqual(edit_distance('pace', 'space'), 1)
-			self.assertEqual(edit_distance('son', 'sloan'), 2)
-			self.assertEqual(edit_distance('kin', 'kevin'), 2)
+	#Tests insertion/deletion
+	def test_edit_insertion(self):
+		self.assertEqual(edit_distance('pace', 'space'), 1)
+		self.assertEqual(edit_distance('son', 'sloan'), 2)
+		self.assertEqual(edit_distance('kin', 'kevin'), 2)
 
-		#Tests case-insensitivity
-		def test_edit_case_insensitive(self):
-			self.assertEqual(edit_distance('kitten', 'Knitting'),3)
-			self.assertEqual(edit_distance('blaCk', 'Black'), 0)
+	#Tests case-insensitivity
+	def test_edit_case_insensitive(self):
+		self.assertEqual(edit_distance('kitten', 'Knitting'),3)
+		self.assertEqual(edit_distance('blaCk', 'Black'), 0)
 
-		#Tests random word
-		def test_random_words(self):
-			test_cases = zip(['islander','mart','kitten','intention'], ['slander', 'karma','sitting','execution'])
-			test_results = [1,3,3,5]
-			for i in range(len(test_cases)):
-				self.assertEqual(edit_distance(test_cases[i][0], test_cases[i][1]), test_results[i])
+	#Tests selected word
+	def test_selected_words(self):
+		test_cases = zip(['islander','mart','kitten','intention'], 
+			             ['slander', 'karma','sitting','execution'],
+			             [1,3,3,5])
+		for i in range(len(test_cases)):
+			self.assertEqual(edit_distance(test_cases[i][0], test_cases[i][1]), test_cases[i][2])
 
 if __name__ == '__main__':
     unittest.main()
-
-		
--- edit_distance.py
+++ edit_distance.py
@@ -4,25 +4,19 @@ def edit_distance(s1, s2):
     s1 = s1.lower()
     s2 = s2.lower()
 
-    #Construct table that displays DAG
-    dag = {}
-
+    #Construct matrix that displays DAG
+    dag = [[0 for x in range(len(s2) + 1)] for y in range(len(s1) + 1)]
+    
     #Horizontal movement represents insertion
     #Vertical movement represents deletion
-    #Diagnoal movement represents copy & substitution
-    for i in range(len(s1)+1): 
-        dag[i,0]=i
-    for j in range(len(s2)+1): 
-        dag[0,j]=j    
-    for i in range(1, len(s1)+1):
-        for j in range(1, len(s2)+1):
-            cost = None
-            if s1[i-1] == s2[j-1]:
-                cost = 0
-            else: 
-                cost = 1
-            dag[i,j] = min(dag[i, j-1]+1, dag[i-1, j]+1, cost+dag[i-1, j-1])
+    #Diagonal movement represents copy & substitution
+    for i in range(len(s1) + 1): 
+        dag[i][0]=i
+    for j in range(len(s2) + 1): 
+        dag[0][j]=j    
+    for i in range(1, len(s1) + 1):
+        for j in range(1, len(s2) + 1):            
+            cost = 0 if s1[i-1] == s2[j-1] else 1
+            dag[i][j] = min(dag[i][j-1] + 1, dag[i-1][j] + 1, cost + dag[i-1][j-1])
 
-    return dag[i,j]
-
-
+    return dag[i][j]
